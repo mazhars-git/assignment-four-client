69911c7319a272ce2cd27ea898edb685822ac1fd
mazhars-git <mazharulislam611@gmail.com>
2025-02-20 05:45:25 +0600

    featured

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import FeaturedProducts from "./Home/FeaturedProducts";
 import Header from "./Home/Header";
 
 const HomePage = () => {
   return (
     <div className="min-h-screen">
       <Header />
+      <FeaturedProducts />
     </div>
   );
 };
---
+++ src/pages/Home/FeaturedProducts.tsx
@@ -0,0 +1,65 @@
+import { NavLink } from "react-router-dom";
+
+const FeaturedProducts = () => {
+  return (
+    <div className="py-5">
+      <h1 className="text-5xl font-bold text-orange-900 text-center my-3 uppercase">
+        Features
+      </h1>
+      <div className="flex grid-cols-3 py-5 items-center justify-center space-x-4">
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <figure>
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <figure>
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <figure>
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="text-center">
+        <NavLink to="/allProducts">
+          <button className="btn btn-warning rounded-full">View All</button>
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProducts;
